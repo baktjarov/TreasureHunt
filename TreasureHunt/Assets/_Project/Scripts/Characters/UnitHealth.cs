7ad8d722c4bd14278466934c126f24e60c12fc70
baktjarov <baktyiarovaitegin@gmail.com>
2024-04-27 12:52:14 -0600

    Death system

---
+++ TreasureHunt/Assets/_Project/Scripts/Characters/UnitHealth.cs
@@ -0,0 +1,32 @@
+using Interfaces;
+using System;
+using UnityEngine;
+
+namespace Characters
+{
+    public class UnitHealth : MonoBehaviour, IHealth
+    {
+        public Action onDie { get; set; }
+        public bool isAlive { get; private set; }
+
+        public float currentHealth { get; private set; }
+        public float maxHealth { get; private set; } = 100;
+
+        private void Start()
+        {
+            if (currentHealth < maxHealth) { currentHealth = maxHealth; }
+        }
+
+        public void TakeDamage(float damage)
+        {
+            currentHealth = currentHealth - damage;
+
+            if (currentHealth <= 0)
+            {
+                onDie?.Invoke();
+                Debug.Log("die");
+                return;
+            }
+        }
+    }
+}
--- TreasureHunt/Assets/_Project/Scripts/StateMachine/Base/UnitStateMachineBase.cs
+++ TreasureHunt/Assets/_Project/Scripts/StateMachine/Base/UnitStateMachineBase.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Characters;
 using Sensors;
 using TagComponents;
 using UnityEngine;
